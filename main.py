7fdbe5a0084e109ec573a248dfc6267466826fc4
Lyn Aguayo <lyn.aguayoc@gmail.com>
2026-01-15 19:01:43 +0100

    add examples assets

--- main.py
+++ main.py
@@ -1,144 +1,160 @@
-GAME_STATE_MENU = 0
-GAME_STATE_CHAR_SELECT = 1
-GAME_STATE_NAME_INPUT = 2
-GAME_STATE_PLAYING = 3
-
-game_state = GAME_STATE_MENU
-player_name = "HUNTER"
-selected_character = 0
-score = 0
-game_time = 180
-
-player_sprite: Sprite = None
-main_menu: miniMenu.MenuSprite = None
-char_menu: miniMenu.MenuSprite = None
-name_menu: miniMenu.MenuSprite = None
-
-scene.set_background_color(15)
-
-def show_main_menu():
-    global game_state, main_menu
-    game_state = GAME_STATE_MENU
-    
-    main_menu = miniMenu.create_menu(
-        miniMenu.create_menu_item("HISTORIA"),
-        miniMenu.create_menu_item("VERSUS"),
-        miniMenu.create_menu_item("CREDITOS")
-    )
-    main_menu.set_position(80, 60)
-    main_menu.set_menu_style_property(miniMenu.MenuStyleProperty.WIDTH, 80)
-    main_menu.set_menu_style_property(miniMenu.MenuStyleProperty.HEIGHT, 50)
-    main_menu.set_style_property(miniMenu.StyleKind.DEFAULT, miniMenu.StyleProperty.BACKGROUND, 15)
-    main_menu.set_style_property(miniMenu.StyleKind.DEFAULT, miniMenu.StyleProperty.FOREGROUND, 1)
-    main_menu.set_style_property(miniMenu.StyleKind.SELECTED, miniMenu.StyleProperty.BACKGROUND, 8)
-    main_menu.set_style_property(miniMenu.StyleKind.SELECTED, miniMenu.StyleProperty.FOREGROUND, 1)
-    main_menu.on_button_pressed(controller.A, on_main_menu_select)
-
-def on_main_menu_select(selection, selectedIndex):
-    main_menu.close()
-    if selectedIndex == 0:
-        show_character_select()
-    elif selectedIndex == 1:
-        game.splash("VERSUS", "Proximamente!")
-        show_main_menu()
-    elif selectedIndex == 2:
-        game.splash("CYBER-NEON", "Creado por New")
-        show_main_menu()
-
-def show_character_select():
-    global game_state, char_menu
-    game_state = GAME_STATE_CHAR_SELECT
-    
-    sprites.destroy_all_sprites_of_kind(SpriteKind.player)
-    
-    ander_preview = sprites.create(assets.image("""ander_idle"""), SpriteKind.player)
-    ander_preview.set_position(40, 50)
-    
-    kira_preview = sprites.create(assets.image("""kira_idle"""), SpriteKind.player)
-    kira_preview.set_position(80, 50)
-    
-    random_preview = sprites.create(assets.image("""random_idle"""), SpriteKind.player)
-    random_preview.set_position(120, 50)
-    
-    char_menu = miniMenu.create_menu(
-        miniMenu.create_menu_item("ANDER"),
-        miniMenu.create_menu_item("KIRA"),
-        miniMenu.create_menu_item("RANDOM")
-    )
-    char_menu.set_position(80, 100)
-    char_menu.set_menu_style_property(miniMenu.MenuStyleProperty.WIDTH, 100)
-    char_menu.set_menu_style_property(miniMenu.MenuStyleProperty.HEIGHT, 30)
-    char_menu.set_style_property(miniMenu.StyleKind.DEFAULT, miniMenu.StyleProperty.BACKGROUND, 15)
-    char_menu.set_style_property(miniMenu.StyleKind.DEFAULT, miniMenu.StyleProperty.FOREGROUND, 1)
-    char_menu.set_style_property(miniMenu.StyleKind.SELECTED, miniMenu.StyleProperty.BACKGROUND, 8)
-    char_menu.set_style_property(miniMenu.StyleKind.SELECTED, miniMenu.StyleProperty.FOREGROUND, 1)
-    char_menu.on_button_pressed(controller.A, on_char_select)
-
-def on_char_select(selection, selectedIndex):
-    global selected_character
-    selected_character = selectedIndex
-    char_menu.close()
-    sprites.destroy_all_sprites_of_kind(SpriteKind.player)
-    show_name_input()
-
-def show_name_input():
-    global game_state, name_menu
-    game_state = GAME_STATE_NAME_INPUT
-    
-    name_menu = miniMenu.create_menu(
-        miniMenu.create_menu_item("HUNTER"),
-        miniMenu.create_menu_item("CYBER"),
-        miniMenu.create_menu_item("NEON"),
-        miniMenu.create_menu_item("GLITCH"),
-        miniMenu.create_menu_item("SHADOW")
-    )
-    name_menu.set_position(80, 60)
-    name_menu.set_menu_style_property(miniMenu.MenuStyleProperty.WIDTH, 80)
-    name_menu.set_menu_style_property(miniMenu.MenuStyleProperty.HEIGHT, 70)
-    name_menu.set_style_property(miniMenu.StyleKind.DEFAULT, miniMenu.StyleProperty.BACKGROUND, 15)
-    name_menu.set_style_property(miniMenu.StyleKind.DEFAULT, miniMenu.StyleProperty.FOREGROUND, 1)
-    name_menu.set_style_property(miniMenu.StyleKind.SELECTED, miniMenu.StyleProperty.BACKGROUND, 8)
-    name_menu.set_style_property(miniMenu.StyleKind.SELECTED, miniMenu.StyleProperty.FOREGROUND, 1)
-    name_menu.on_button_pressed(controller.A, on_name_select)
-
-def on_name_select(selection, selectedIndex):
-    global player_name
-    names = ["HUNTER", "CYBER", "NEON", "GLITCH", "SHADOW"]
-    player_name = names[selectedIndex]
-    name_menu.close()
-    start_gameplay()
+def on_countdown_end():
+    if score >= 500:
+        game.splash("VICTORIA!", "Score: " + ("" + str(score)))
+    else:
+        game.splash("GAME OVER", "Score: " + ("" + str(score)))
+    game.reset()
+info.on_countdown_end(on_countdown_end)
 
 def start_gameplay():
-    global game_state, player_sprite, score, game_time
-    
+    global game_state, score, game_time, player_sprite
     game_state = GAME_STATE_PLAYING
     score = 0
     game_time = 180
-    
     sprites.destroy_all_sprites_of_kind(SpriteKind.player)
-    
     if selected_character == 0:
-        player_sprite = sprites.create(assets.image("""ander_idle"""), SpriteKind.player)
+        player_sprite = sprites.create(assets.image("""
+            ander_idle
+            """), SpriteKind.player)
     elif selected_character == 1:
-        player_sprite = sprites.create(assets.image("""kira_idle"""), SpriteKind.player)
+        player_sprite = sprites.create(assets.image("""
+            kira_idle
+            """), SpriteKind.player)
     else:
-        player_sprite = sprites.create(assets.image("""random_idle"""), SpriteKind.player)
-    
+        player_sprite = sprites.create(assets.image("""
+            random_idle
+            """), SpriteKind.player)
     player_sprite.set_position(80, 60)
     player_sprite.set_stay_in_screen(True)
     controller.move_sprite(player_sprite, 100, 100)
-    
     info.set_score(0)
     info.start_countdown(180)
-
-def on_countdown_end():
-    if score >= 500:
-        game.splash("VICTORIA!", "Score: " + str(score))
-    else:
-        game.splash("GAME OVER", "Score: " + str(score))
-    game.reset()
-
-info.on_countdown_end(on_countdown_end)
-
+def show_main_menu():
+    global game_state, main_menu
+    game_state = GAME_STATE_MENU
+    main_menu = miniMenu.create_menu(miniMenu.create_menu_item("HISTORIA"),
+        miniMenu.create_menu_item("VERSUS"),
+        miniMenu.create_menu_item("CREDITOS"))
+    main_menu.set_position(80, 60)
+    main_menu.set_menu_style_property(miniMenu.MenuStyleProperty.WIDTH, 80)
+    main_menu.set_menu_style_property(miniMenu.MenuStyleProperty.HEIGHT, 50)
+    main_menu.set_style_property(miniMenu.StyleKind.DEFAULT,
+        miniMenu.StyleProperty.BACKGROUND,
+        15)
+    main_menu.set_style_property(miniMenu.StyleKind.DEFAULT,
+        miniMenu.StyleProperty.FOREGROUND,
+        1)
+    main_menu.set_style_property(miniMenu.StyleKind.SELECTED,
+        miniMenu.StyleProperty.BACKGROUND,
+        8)
+    main_menu.set_style_property(miniMenu.StyleKind.SELECTED,
+        miniMenu.StyleProperty.FOREGROUND,
+        1)
+    
+    def on_button_pressed(selection, selectedIndex):
+        main_menu.close()
+        if selectedIndex == 0:
+            show_character_select()
+        elif selectedIndex == 1:
+            game.splash("VERSUS", "Proximamente!")
+            show_main_menu()
+        elif selectedIndex == 2:
+            game.splash("CYBER-NEON", "Creado por New")
+            show_main_menu()
+    main_menu.on_button_pressed(controller.A, on_button_pressed)
+    
+def show_character_select():
+    global game_state, ander_preview, kira_preview, random_preview, char_menu
+    game_state = GAME_STATE_CHAR_SELECT
+    sprites.destroy_all_sprites_of_kind(SpriteKind.player)
+    ander_preview = sprites.create(assets.image("""jugador_vermell"""), SpriteKind.player)
+    ander_preview.set_position(40, 50)
+    kira_preview = sprites.create(assets.image("""jugador_vermell"""), SpriteKind.player)
+    kira_preview.set_position(80, 50)
+    random_preview = sprites.create(assets.image("""jugador_vermell"""), SpriteKind.player)
+    random_preview.set_position(120, 50)
+    char_menu = miniMenu.create_menu(miniMenu.create_menu_item("ANDER"),
+        miniMenu.create_menu_item("KIRA"),
+        miniMenu.create_menu_item("RANDOM"))
+    char_menu.set_position(80, 100)
+    char_menu.set_menu_style_property(miniMenu.MenuStyleProperty.WIDTH, 100)
+    char_menu.set_menu_style_property(miniMenu.MenuStyleProperty.HEIGHT, 30)
+    char_menu.set_style_property(miniMenu.StyleKind.DEFAULT,
+        miniMenu.StyleProperty.BACKGROUND,
+        15)
+    char_menu.set_style_property(miniMenu.StyleKind.DEFAULT,
+        miniMenu.StyleProperty.FOREGROUND,
+        1)
+    char_menu.set_style_property(miniMenu.StyleKind.SELECTED,
+        miniMenu.StyleProperty.BACKGROUND,
+        8)
+    char_menu.set_style_property(miniMenu.StyleKind.SELECTED,
+        miniMenu.StyleProperty.FOREGROUND,
+        1)
+    
+    def on_button_pressed2(selection2, selectedIndex2):
+        global selected_character
+        selected_character = selectedIndex2
+        char_menu.close()
+        sprites.destroy_all_sprites_of_kind(SpriteKind.player)
+        show_name_input()
+    char_menu.on_button_pressed(controller.A, on_button_pressed2)
+    
+def show_name_input():
+    global game_state, name_menu
+    game_state = GAME_STATE_NAME_INPUT
+    name_menu = miniMenu.create_menu(miniMenu.create_menu_item("HUNTER"),
+        miniMenu.create_menu_item("CYBER"),
+        miniMenu.create_menu_item("NEON"),
+        miniMenu.create_menu_item("GLITCH"),
+        miniMenu.create_menu_item("SHADOW"))
+    name_menu.set_position(80, 60)
+    name_menu.set_menu_style_property(miniMenu.MenuStyleProperty.WIDTH, 80)
+    name_menu.set_menu_style_property(miniMenu.MenuStyleProperty.HEIGHT, 70)
+    name_menu.set_style_property(miniMenu.StyleKind.DEFAULT,
+        miniMenu.StyleProperty.BACKGROUND,
+        15)
+    name_menu.set_style_property(miniMenu.StyleKind.DEFAULT,
+        miniMenu.StyleProperty.FOREGROUND,
+        1)
+    name_menu.set_style_property(miniMenu.StyleKind.SELECTED,
+        miniMenu.StyleProperty.BACKGROUND,
+        8)
+    name_menu.set_style_property(miniMenu.StyleKind.SELECTED,
+        miniMenu.StyleProperty.FOREGROUND,
+        1)
+    
+    def on_button_pressed3(selection3, selectedIndex3):
+        global names, player_name
+        names = ["HUNTER", "CYBER", "NEON", "GLITCH", "SHADOW"]
+        player_name = names[selectedIndex3]
+        name_menu.close()
+        start_gameplay()
+    name_menu.on_button_pressed(controller.A, on_button_pressed3)
+    
+names: List[str] = []
+name_menu: miniMenu.MenuSprite = None
+char_menu: miniMenu.MenuSprite = None
+random_preview: Sprite = None
+kira_preview: Sprite = None
+ander_preview: Sprite = None
+main_menu: miniMenu.MenuSprite = None
+player_sprite: Sprite = None
+selected_character = 0
+score = 0
+game_time = 0
+player_name = ""
+GAME_STATE_MENU = 0
+game_state = 0
+GAME_STATE_PLAYING = 0
+GAME_STATE_NAME_INPUT = 0
+GAME_STATE_CHAR_SELECT = 0
+GAME_STATE_CHAR_SELECT = 1
+GAME_STATE_NAME_INPUT = 2
+GAME_STATE_PLAYING = 3
+game_state = GAME_STATE_MENU
+player_name = "HUNTER"
+game_time = 180
+scene.set_background_color(15)
 game.splash("CYBER-NEON", "VIRUS HUNT")
 show_main_menu()
--- main.ts
+++ main.ts
@@ -1,36 +1,43 @@
-info.onCountdownEnd(function () {
+info.onCountdownEnd(function on_countdown_end() {
     if (score >= 500) {
-        game.splash("VICTORIA!", "Score: " + ("" + score))
+        game.splash("VICTORIA!", "Score: " + ("" + ("" + score)))
     } else {
-        game.splash("GAME OVER", "Score: " + ("" + score))
+        game.splash("GAME OVER", "Score: " + ("" + ("" + score)))
     }
+    
     game.reset()
 })
-function start_gameplay () {
+function start_gameplay() {
+    
     game_state = GAME_STATE_PLAYING
     score = 0
     game_time = 180
     sprites.destroyAllSpritesOfKind(SpriteKind.Player)
     if (selected_character == 0) {
-        player_sprite = sprites.create(assets.image`ander_idle`, SpriteKind.Player)
+        player_sprite = sprites.create(assets.image`
+            ander_idle
+            `, SpriteKind.Player)
     } else if (selected_character == 1) {
-        player_sprite = sprites.create(assets.image`kira_idle`, SpriteKind.Player)
+        player_sprite = sprites.create(assets.image`
+            kira_idle
+            `, SpriteKind.Player)
     } else {
-        player_sprite = sprites.create(assets.image`random_idle`, SpriteKind.Player)
+        player_sprite = sprites.create(assets.image`
+            random_idle
+            `, SpriteKind.Player)
     }
+    
     player_sprite.setPosition(80, 60)
     player_sprite.setStayInScreen(true)
     controller.moveSprite(player_sprite, 100, 100)
     info.setScore(0)
     info.startCountdown(180)
 }
-function show_main_menu () {
+
+function show_main_menu() {
+    
     game_state = GAME_STATE_MENU
-    main_menu = miniMenu.createMenu(
-    miniMenu.createMenuItem("HISTORIA"),
-    miniMenu.createMenuItem("VERSUS"),
-    miniMenu.createMenuItem("CREDITOS")
-    )
+    main_menu = miniMenu.createMenu(miniMenu.createMenuItem("HISTORIA"), miniMenu.createMenuItem("VERSUS"), miniMenu.createMenuItem("CREDITOS"))
     main_menu.setPosition(80, 60)
     main_menu.setMenuStyleProperty(miniMenu.MenuStyleProperty.Width, 80)
     main_menu.setMenuStyleProperty(miniMenu.MenuStyleProperty.Height, 50)
@@ -38,7 +45,7 @@ function show_main_menu () {
     main_menu.setStyleProperty(miniMenu.StyleKind.Default, miniMenu.StyleProperty.Foreground, 1)
     main_menu.setStyleProperty(miniMenu.StyleKind.Selected, miniMenu.StyleProperty.Background, 8)
     main_menu.setStyleProperty(miniMenu.StyleKind.Selected, miniMenu.StyleProperty.Foreground, 1)
-    main_menu.onButtonPressed(controller.A, function (selection, selectedIndex) {
+    main_menu.onButtonPressed(controller.A, function on_button_pressed(selection: any, selectedIndex: any) {
         main_menu.close()
         if (selectedIndex == 0) {
             show_character_select()
@@ -49,22 +56,21 @@ function show_main_menu () {
             game.splash("CYBER-NEON", "Creado por New")
             show_main_menu()
         }
+        
     })
 }
-function show_character_select () {
+
+function show_character_select() {
+    
     game_state = GAME_STATE_CHAR_SELECT
     sprites.destroyAllSpritesOfKind(SpriteKind.Player)
-    ander_preview = sprites.create(assets.image`ander_idle`, SpriteKind.Player)
+    ander_preview = sprites.create(assets.image`jugador_vermell`, SpriteKind.Player)
     ander_preview.setPosition(40, 50)
-    kira_preview = sprites.create(assets.image`kira_idle`, SpriteKind.Player)
+    kira_preview = sprites.create(assets.image`jugador_vermell`, SpriteKind.Player)
     kira_preview.setPosition(80, 50)
-    random_preview = sprites.create(assets.image`random_idle`, SpriteKind.Player)
+    random_preview = sprites.create(assets.image`jugador_vermell`, SpriteKind.Player)
     random_preview.setPosition(120, 50)
-    char_menu = miniMenu.createMenu(
-    miniMenu.createMenuItem("ANDER"),
-    miniMenu.createMenuItem("KIRA"),
-    miniMenu.createMenuItem("RANDOM")
-    )
+    char_menu = miniMenu.createMenu(miniMenu.createMenuItem("ANDER"), miniMenu.createMenuItem("KIRA"), miniMenu.createMenuItem("RANDOM"))
     char_menu.setPosition(80, 100)
     char_menu.setMenuStyleProperty(miniMenu.MenuStyleProperty.Width, 100)
     char_menu.setMenuStyleProperty(miniMenu.MenuStyleProperty.Height, 30)
@@ -72,22 +78,19 @@ function show_character_select () {
     char_menu.setStyleProperty(miniMenu.StyleKind.Default, miniMenu.StyleProperty.Foreground, 1)
     char_menu.setStyleProperty(miniMenu.StyleKind.Selected, miniMenu.StyleProperty.Background, 8)
     char_menu.setStyleProperty(miniMenu.StyleKind.Selected, miniMenu.StyleProperty.Foreground, 1)
-    char_menu.onButtonPressed(controller.A, function (selection, selectedIndex) {
-        selected_character = selectedIndex
+    char_menu.onButtonPressed(controller.A, function on_button_pressed2(selection2: any, selectedIndex2: number) {
+        
+        selected_character = selectedIndex2
         char_menu.close()
         sprites.destroyAllSpritesOfKind(SpriteKind.Player)
         show_name_input()
     })
 }
-function show_name_input () {
+
+function show_name_input() {
+    
     game_state = GAME_STATE_NAME_INPUT
-    name_menu = miniMenu.createMenu(
-    miniMenu.createMenuItem("HUNTER"),
-    miniMenu.createMenuItem("CYBER"),
-    miniMenu.createMenuItem("NEON"),
-    miniMenu.createMenuItem("GLITCH"),
-    miniMenu.createMenuItem("SHADOW")
-    )
+    name_menu = miniMenu.createMenu(miniMenu.createMenuItem("HUNTER"), miniMenu.createMenuItem("CYBER"), miniMenu.createMenuItem("NEON"), miniMenu.createMenuItem("GLITCH"), miniMenu.createMenuItem("SHADOW"))
     name_menu.setPosition(80, 60)
     name_menu.setMenuStyleProperty(miniMenu.MenuStyleProperty.Width, 80)
     name_menu.setMenuStyleProperty(miniMenu.MenuStyleProperty.Height, 70)
@@ -95,27 +98,23 @@ function show_name_input () {
     name_menu.setStyleProperty(miniMenu.StyleKind.Default, miniMenu.StyleProperty.Foreground, 1)
     name_menu.setStyleProperty(miniMenu.StyleKind.Selected, miniMenu.StyleProperty.Background, 8)
     name_menu.setStyleProperty(miniMenu.StyleKind.Selected, miniMenu.StyleProperty.Foreground, 1)
-    name_menu.onButtonPressed(controller.A, function (selection, selectedIndex) {
-        names = [
-        "HUNTER",
-        "CYBER",
-        "NEON",
-        "GLITCH",
-        "SHADOW"
-        ]
-        player_name = names[selectedIndex]
+    name_menu.onButtonPressed(controller.A, function on_button_pressed3(selection3: any, selectedIndex3: number) {
+        
+        names = ["HUNTER", "CYBER", "NEON", "GLITCH", "SHADOW"]
+        player_name = names[selectedIndex3]
         name_menu.close()
         start_gameplay()
     })
 }
-let names: string[] = []
-let name_menu: miniMenu.MenuSprite = null
-let char_menu: miniMenu.MenuSprite = null
-let random_preview: Sprite = null
-let kira_preview: Sprite = null
-let ander_preview: Sprite = null
-let main_menu: miniMenu.MenuSprite = null
-let player_sprite: Sprite = null
+
+let names : string[] = []
+let name_menu : miniMenu.MenuSprite = null
+let char_menu : miniMenu.MenuSprite = null
+let random_preview : Sprite = null
+let kira_preview : Sprite = null
+let ander_preview : Sprite = null
+let main_menu : miniMenu.MenuSprite = null
+let player_sprite : Sprite = null
 let selected_character = 0
 let score = 0
 let game_time = 0
